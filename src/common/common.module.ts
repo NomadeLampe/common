32d6e9fcbbc6e8ccf5ad25f3568f198df1b88564
admin <admin@omp-transport.fr>
2017-10-24 11:10:44 +0200

    Création du module common

---
+++ src/common/common.module.ts
@@ -0,0 +1,11 @@
+import {NgModule} from '@angular/core';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CommonModule],
+  bootstrap: [CommonModule]
+})
+export class CommonModule {
+}
