b8003a83b9985120b839022ca1019f9402279478
admin <admin@omp-transport.fr>
2017-10-24 11:21:24 +0200

    Ajout et configuration de ngx-translate

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,8 +1,22 @@
 import {NgModule} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http, './assets/i18n/');
+}
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   declarations: [CommonModule],
   bootstrap: [CommonModule]
